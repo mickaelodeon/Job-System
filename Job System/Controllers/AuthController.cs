2360cb7069afb686ee06d5014919424d7a1c4d12
John Michael Eborda <johnmichaeleborda79@gmail.com>
2024-12-08 20:57:17 +0800

    afer first checking

---
+++ Job System/Controllers/AuthController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using JobPortal.Models;
+
+namespace JobPortal.Controllers
+{
+    public class AuthController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        public IActionResult ForgotPassword()
+        {
+            return View();
+        }
+
+        public IActionResult CandidateRegister()
+        {
+            return View();
+        }
+
+        public IActionResult CompanyRegister()
+        {
+            return View();
+        }
+    }
+}
